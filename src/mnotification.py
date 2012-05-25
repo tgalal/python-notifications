dc8e3676253ba47402a23c38b395f13cf346c448
tarekg <tarekg@tarek-gentoo.(none)>
2012-05-25 03:56:52 +0200

    fixed typo

--- src/mnotification.py
+++ src/mnotification.py
@@ -11,13 +11,13 @@ under the terms of the GNU Lesser General Public License as published by the
 Free Software Foundation, either version 3 of the License, or (at your option) 
 any later version.
 
-Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY 
+python-notifications is distributed in the hope that it will be useful, but WITHOUT ANY 
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
 A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
 details.
 
 You should have received a copy of the GNU General Public License along with 
-Wazapp. If not, see http://www.gnu.org/licenses/.
+python-notifications. If not, see http://www.gnu.org/licenses/.
 '''
 
 '''
