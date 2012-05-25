9b8c67a1115986db276f04e4e09259dde6c5e042
tarekg <tarekg@tarek-gentoo.(none)>
2012-05-25 03:52:24 +0200

    license ammend

--- src/mnotification.py
+++ src/mnotification.py
@@ -3,13 +3,21 @@
 '''
 Copyright (c) 2012, Tarek Galal <tare2.galal@gmail.com>
 
-This file is part of python-notifications, a library that allows you to post notifications on Harmattan platform
+This file is part of python-notifications, a library that allows you to post 
+notifications on Harmattan platform
 
-python-notifications is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+python-notifications is free software: you can redistribute it and/or modify it 
+under the terms of the GNU Lesser General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
 
-Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+details.
 
-You should have received a copy of the GNU General Public License along with Wazapp. If not, see http://www.gnu.org/licenses/.
+You should have received a copy of the GNU General Public License along with 
+Wazapp. If not, see http://www.gnu.org/licenses/.
 '''
 
 '''
